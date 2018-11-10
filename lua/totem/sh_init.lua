03ef00d444dca4ff8d3c0f60450c00bb74d6c67e
Alf21 <4lf-mueller@gmx.de>
2018-11-10 22:26:27 +0100

    Fix for sprint add-ons

--- lua/totem/sh_init.lua
+++ lua/totem/sh_init.lua
@@ -1,30 +1,32 @@
 TTT2Totem = {}
 
-hook.Add("TTTPlayerSpeedModifier", "TTT2TotemSpeed", function(ply)
+hook.Add("TTTPlayerSpeedModifier", "TTT2TotemSpeed", function(ply, slowed, mv)
 	if not GetConVar("ttt2_totem"):GetBool() or not TTT2Totem.AnyTotems then return end
 
 	local rs = GetRoundState()
 
 	if rs == ROUND_ACTIVE or rs == ROUND_POST then
+		local mul = 1
 
 		if not ply.PlacedTotem then
-			return 0.4
+			mul = 0.4
 		else
 			local Totem = ply:GetTotem()
 
 			if IsValid(Totem) then
 				local distance = Totem:GetPos():Distance(ply:GetPos())
 				if distance >= 2500 then
-					return math.Round(math.Remap(distance, 2500, 5000, 1, 0.75), 2)
+					mul = math.Round(math.Remap(distance, 2500, 5000, 1, 0.75), 2)
 				elseif distance <= 1000 then
-					return 1.25
-				elseif distance > 1000 and distance < 2500 then
-					return 1
+					mul = 1.25
 				end
 			else
-				return 0.75
+				mul = 0.75
 			end
 		end
+
+		mv:SetMaxClientSpeed(mv:GetMaxClientSpeed() * mul)
+		mv:SetMaxSpeed(mv:GetMaxSpeed() * mul)
 	end
 end)
 
