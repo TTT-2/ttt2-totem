47f2499629437fe0d7edbdd089f919a3bccd4755
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-10-31 11:57:06 +0100

    Language: French translation (#12)
    French translation

---
+++ lua/terrortown/lang/french/totem.lua
@@ -0,0 +1,34 @@
+local L = LANG.GetLanguageTableReference("french")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[TOTEMHUNTER.name] = "Chasseur de Totem"
+L["info_popup_" .. TOTEMHUNTER.name] = [[Vous êtes un Chasseur de Totem! Essayez de détruire quelques Totems pour obtenir des crédits d'équipement!]]
+L["body_found_" .. TOTEMHUNTER.abbr] = "C'était un Chasseur de Totem!"
+L["search_role_" .. TOTEMHUNTER.abbr] = "Cette personne était un Chasseur de Totem!"
+L["target_" .. TOTEMHUNTER.name] = "Chasseur de Totem"
+L["ttt2_desc_" .. TOTEMHUNTER.name] = [[Le Chasseur de Totem est un traître qui a pour but de tuer tous les autres rôles sauf les autres traître. Le chasseur de Totems est capable de détruire les totems de ses ennemis pour recevoir des crédits d'équipement.]]
+
+-- OTHER LANGUAGE STRINGS
+L["totem_name"] = "Totem"
+L["totem_owner"] = "C'est le Totem de {name}"
+L["totem_already_placed"] = "Vous avez déjà placé un Totem!"
+L["totem_place_ground_needed"] = "Vous devez toucher sol lorsque vous placez votre Totem!"
+L["totem_placed"] = "Totem placé avec succès!"
+L["totem_picked_up"] = "Vous avez ramassé votre Totem!"
+L["totem_destroyed"] = "Un Totem a été détruit!"
+L["totem_slow"] = "Vous êtes visiblement plus lent à cause de la destruction de votre Totem!"
+L["totem_already_picked_up"] = "Vous pouvez récupérer votre Totem uniquement {num} fois!"
+L["totem_already_no_pickup"] = "Vous ne pouvez pas récupérer votre Totem!"
+L["totem_all_destroyed"] = "Tous les Totems ont été détruits!"
+L["totem_other_terrorist"] = "C'est le Totem d'un autre terroriste"
+L["totem_own_totem"] = "C'est votre Totem"
+L["totem_teammate_totem"] = "C'est le Totem d'un coéquipier"
+L["totem_destroy_totem"] = "Détruisez ce Totem avec votre couteau!"
+
+L["weapon_thunter_knife_name"] = "Couteau du Chasseur de Totem"
+
+L["submenu_addons_totem_title"] = "TTT2 Totem"
+L["header_addons_totem"] = "Paramètres généraux du mode TTT2 Totem"
+L["label_totem_auto_place_enable"] = "Le système devrait-il essayer de placer automatiquement le Totem au début du round?"
+
+L["header_bindings_totem"] = "TTT2 Totem"
